7f259d9ed208c41d2bd7da4cffaef7b533721453
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-09 12:31:03 +0900

    ルーティングを定義。動作確認おk。

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css';
 import { Button, ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
+
 import theme from './theme/theme';
+import { Router } from './router/Router';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Button colorScheme="blackAlpha">test</Button>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
---
+++ src/components/pages/UserManagement.tsx
@@ -0,0 +1,5 @@
+import { memo, VFC } from 'react';
+
+export const UserManagement: VFC = memo(() => {
+  return <div>ユーザ管理 page</div>;
+});
