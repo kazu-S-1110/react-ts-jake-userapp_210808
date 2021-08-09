f31d9a66f21dd7bb18ba691c0bde073332b7796e
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-09 21:54:28 +0900

    chakaraのDrawerの実装を学習。useDiscloseが便利すぎ〜。

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,43 +1,71 @@
-import { IconButton } from '@chakra-ui/button';
+import { Button, IconButton } from '@chakra-ui/button';
+import { useDisclosure } from '@chakra-ui/hooks';
 import { HamburgerIcon } from '@chakra-ui/icons';
 import { Box, Flex, Heading, Link } from '@chakra-ui/layout';
+import {
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerOverlay,
+} from '@chakra-ui/modal';
 import { memo, VFC } from 'react';
 
 export const Header: VFC = memo(() => {
+  const { isOpen, onClose, onOpen } = useDisclosure();
   return (
-    <Flex
-      as="nav"
-      bg="teal.500"
-      color="gray.50"
-      alignItems="center"
-      justifyContent="space-between"
-      padding={{ base: '3', md: '5' }}
-    >
-      <Flex align="center" as="a" mr="8" _hover={{ cursor: 'pointer' }}>
-        <Heading as="h1" fontSize={{ base: 'md', md: 'lg' }}>
-          hogehoge
-        </Heading>
-      </Flex>
+    <>
       <Flex
+        as="nav"
+        bg="teal.500"
+        color="gray.50"
         alignItems="center"
-        fontSize="sm"
-        flexGrow={2}
-        // chakrauiのレスポンシブ対応は以下のようにすると良い
-        display={{ base: 'none', md: 'flex' }}
+        justifyContent="space-between"
+        padding={{ base: '3', md: '5' }}
       >
-        <Box pr="4">
-          <Link>Users</Link>
-        </Box>
-        <Link>Settings</Link>
+        <Flex align="center" as="a" mr="8" _hover={{ cursor: 'pointer' }}>
+          <Heading as="h1" fontSize={{ base: 'md', md: 'lg' }}>
+            hogehoge
+          </Heading>
+        </Flex>
+        <Flex
+          alignItems="center"
+          fontSize="sm"
+          flexGrow={2}
+          // chakrauiのレスポンシブ対応は以下のようにすると良い
+          display={{ base: 'none', md: 'flex' }}
+        >
+          <Box pr="4">
+            <Link>Users</Link>
+          </Box>
+          <Link>Settings</Link>
+        </Flex>
+        <IconButton
+          aria-label="menu_button"
+          icon={<HamburgerIcon />}
+          size="sm"
+          variant="unstyled"
+          // スマホの時はこのタグを表示するコード
+          display={{ base: 'block', md: 'none' }}
+          onClick={onOpen}
+        />
       </Flex>
-      <IconButton
-        aria-label="menu_button"
-        icon={<HamburgerIcon />}
-        size="sm"
-        variant="unstyled"
-        // スマホの時はこのタグを表示するコード
-        display={{ base: 'block', md: 'none' }}
-      />
-    </Flex>
+      <Drawer placement="right" size="xs" onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay>
+          <DrawerContent>
+            <DrawerBody p={0} bg="gray.100">
+              <Button w="100%" my="3">
+                TOP
+              </Button>
+              <Button w="full" my="3">
+                Users
+              </Button>
+              <Button w="full" my="3">
+                Setting
+              </Button>
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    </>
   );
 });
