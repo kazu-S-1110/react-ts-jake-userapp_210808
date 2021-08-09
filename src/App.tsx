15344d0c78d5afcb82865a9d99be0761a7c737fe
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-09 12:19:18 +0900

    chakraでthemeを設定する方法を学習。これいいね♪

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
 import { Button, ChakraProvider } from '@chakra-ui/react';
+import theme from './theme/theme';
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Button colorScheme="blackAlpha">test</Button>
     </ChakraProvider>
   );
