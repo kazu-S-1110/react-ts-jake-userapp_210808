d0aad72a9410c5b808a6e928760bef6c3c943570
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-09 17:50:11 +0900

    ルーティング設定を配列で書いてmapで回してみた。

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,6 +1,7 @@
 import { memo, VFC } from 'react';
 import { Route, Switch } from 'react-router';
 import { Login } from '../components/pages/Login';
+import { HomeRoutes } from './HomeRoutes';
 
 export const Router: VFC = memo(() => {
   return (
@@ -8,6 +9,22 @@ export const Router: VFC = memo(() => {
       <Route exact path="/">
         <Login />
       </Route>
+      <Route
+        path="/home"
+        render={({ match: { url } }) => (
+          <Switch>
+            {HomeRoutes.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
+          </Switch>
+        )}
+      ></Route>
     </Switch>
   );
 });
