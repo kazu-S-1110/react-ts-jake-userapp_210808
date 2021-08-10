98ab9643903b45125fea35ebf2923a72ea5ddc2a
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-10 12:35:16 +0900

    ログイン画面をスタイリング。chakraは偉大。はっきりわかんだね。

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,23 @@
+import { Button } from '@chakra-ui/button';
+import { Input } from '@chakra-ui/input';
+import { Box, Divider, Flex, Heading, Stack } from '@chakra-ui/layout';
 import { memo, VFC } from 'react';
 
 export const Login: VFC = memo(() => {
-  return <div>login page</div>;
+  return (
+    <Flex alignItems="center" justifyContent="center" h="100vh">
+      <Box bg="white" w="sm" p="5" borderRadius="3xl" shadow="2xl">
+        <Heading as="h1" size="lg" textAlign="center">
+          ユーザ管理アプリ
+        </Heading>
+        <Divider my="4" />
+        <Stack spacing="5" py="4" px="10">
+          <Input placeholder="User Id" />
+          <Button bg="blue.400" color="white" _hover={{ opacity: '0.4' }}>
+            Login
+          </Button>
+        </Stack>
+      </Box>
+    </Flex>
+  );
 });
