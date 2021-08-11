4fb55d5ab3c6459767144e27fab7b9eecb72a792
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-12 08:23:57 +0900

    仮設でモーダルを実装。chakraが便利で、、、

--- src/components/organisms/user/UserCard.tsx
+++ src/components/organisms/user/UserCard.tsx
@@ -5,34 +5,38 @@ type Props = {
   imageUrl: string;
   userName: string;
   fullName: string;
+  onClick: () => void;
 };
 
-export const UserCard: VFC<Props> = memo(({ imageUrl, userName, fullName }) => {
-  return (
-    <Box
-      w="260px"
-      h="260px"
-      bg="white"
-      borderRadius="2xl"
-      shadow="md"
-      p={4}
-      _hover={{ cursor: 'pointer', opacity: '0.7' }}
-    >
-      <Stack textAlign="center">
-        <Image
-          borderRadius="full"
-          boxSize="160px"
-          src={imageUrl}
-          alt={userName}
-          m="auto"
-        />
-        <Text fontSize="lg" fontWeight="extrabold">
-          {userName}
-        </Text>
-        <Text fontSize="md" color="gray.600">
-          {fullName}
-        </Text>
-      </Stack>
-    </Box>
-  );
-});
+export const UserCard: VFC<Props> = memo(
+  ({ imageUrl, userName, fullName, onClick }) => {
+    return (
+      <Box
+        w="260px"
+        h="260px"
+        bg="white"
+        borderRadius="2xl"
+        shadow="md"
+        p={4}
+        _hover={{ cursor: 'pointer', opacity: '0.7' }}
+        onClick={onClick}
+      >
+        <Stack textAlign="center">
+          <Image
+            borderRadius="full"
+            boxSize="160px"
+            src={imageUrl}
+            alt={userName}
+            m="auto"
+          />
+          <Text fontSize="lg" fontWeight="extrabold">
+            {userName}
+          </Text>
+          <Text fontSize="md" color="gray.600">
+            {fullName}
+          </Text>
+        </Stack>
+      </Box>
+    );
+  }
+);
--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,13 +1,28 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { Center, Wrap, WrapItem } from '@chakra-ui/layout';
-import { Spinner } from '@chakra-ui/react';
-import { memo, useEffect, VFC } from 'react';
+import { Center, Stack, Wrap } from '@chakra-ui/layout';
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Spinner,
+  useDisclosure,
+  WrapItem,
+} from '@chakra-ui/react';
+import { memo, useCallback, useEffect, VFC } from 'react';
 import { UserCard } from '../organisms/user/UserCard';
 import { useAllUsers } from '../../hooks/useAllUsers';
 
 export const UserManagement: VFC = memo(() => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const { getUsers, loading, users } = useAllUsers();
   useEffect(() => getUsers(), []);
+  const onClickUser = useCallback(() => onOpen(), []);
   return (
     <>
       {loading ? (
@@ -28,11 +43,39 @@ export const UserManagement: VFC = memo(() => {
                 userName={user.username}
                 fullName={user.name}
                 imageUrl="https://source.unsplash.com/random"
+                onClick={onClickUser}
               />
             </WrapItem>
           ))}
         </Wrap>
       )}
+      <Modal isOpen={isOpen} onClose={onClose} motionPreset="slideInRight">
+        <ModalOverlay />
+        <ModalContent pb="5">
+          <ModalHeader>User Detail</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody mx="5">
+            <Stack spacing={5}>
+              <FormControl>
+                <FormLabel>Name</FormLabel>
+                <Input value="hogehoge" isReadOnly />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Full Name</FormLabel>
+                <Input value="hogehoge" isReadOnly />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Mail</FormLabel>
+                <Input value="hogehoge" isReadOnly />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Tel</FormLabel>
+                <Input value="hogehoge" isReadOnly />
+              </FormControl>
+            </Stack>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </>
   );
 });
