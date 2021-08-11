1be635fa1f7f68a27dd556ac906ecfbe4e8b76cb
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-11 12:50:23 +0900

    UserCardをコンポーネント分割。

---
+++ src/components/organisms/user/UserCard.tsx
@@ -0,0 +1,38 @@
+import { Box, Image, Stack, Text } from '@chakra-ui/react';
+import { memo, ReactNode, VFC } from 'react';
+
+type Props = {
+  imageUrl: string;
+  userName: string;
+  fullName: string;
+};
+
+export const UserCard: VFC<Props> = memo(({ imageUrl, userName, fullName }) => {
+  return (
+    <Box
+      w="260px"
+      h="260px"
+      bg="white"
+      borderRadius="2xl"
+      shadow="md"
+      p={4}
+      _hover={{ cursor: 'pointer', opacity: '0.7' }}
+    >
+      <Stack textAlign="center">
+        <Image
+          borderRadius="full"
+          boxSize="160px"
+          src={imageUrl}
+          alt={userName}
+          m="auto"
+        />
+        <Text fontSize="lg" fontWeight="extrabold">
+          {userName}
+        </Text>
+        <Text fontSize="md" color="gray.600">
+          {fullName}
+        </Text>
+      </Stack>
+    </Box>
+  );
+});
--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,36 +1,17 @@
 import { Stack, Wrap, WrapItem } from '@chakra-ui/layout';
 import { Box, Image, Text } from '@chakra-ui/react';
 import { memo, VFC } from 'react';
+import { UserCard } from '../organisms/user/UserCard';
 
 export const UserManagement: VFC = memo(() => {
   return (
     <Wrap p={{ base: 5, md: 10 }}>
       <WrapItem>
-        <Box
-          w="260px"
-          h="260px"
-          bg="white"
-          borderRadius="2xl"
-          shadow="md"
-          p={4}
-          _hover={{ cursor: 'pointer', opacity: '0.7' }}
-        >
-          <Stack textAlign="center">
-            <Image
-              borderRadius="full"
-              boxSize="160px"
-              src="https://source.unsplash.com/random"
-              alt="profile img"
-              m="auto"
-            />
-            <Text fontSize="lg" fontWeight="extrabold">
-              Name
-            </Text>
-            <Text fontSize="md" color="gray.600">
-              Sub Text
-            </Text>
-          </Stack>
-        </Box>
+        <UserCard
+          userName="Name"
+          fullName="fullName"
+          imageUrl="https://source.unsplash.com/random"
+        />
       </WrapItem>
     </Wrap>
   );
