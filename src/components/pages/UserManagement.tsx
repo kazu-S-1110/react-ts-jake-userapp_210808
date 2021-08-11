bb7958343ad3bf9d3c3efd5ec45a11019afe82aa
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-11 12:42:49 +0900

    基本的なユーザカードをスタイリング。

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,37 @@
+import { Stack, Wrap, WrapItem } from '@chakra-ui/layout';
+import { Box, Image, Text } from '@chakra-ui/react';
 import { memo, VFC } from 'react';
 
 export const UserManagement: VFC = memo(() => {
-  return <div>ユーザ管理 page</div>;
+  return (
+    <Wrap p={{ base: 5, md: 10 }}>
+      <WrapItem>
+        <Box
+          w="260px"
+          h="260px"
+          bg="white"
+          borderRadius="2xl"
+          shadow="md"
+          p={4}
+          _hover={{ cursor: 'pointer', opacity: '0.7' }}
+        >
+          <Stack textAlign="center">
+            <Image
+              borderRadius="full"
+              boxSize="160px"
+              src="https://source.unsplash.com/random"
+              alt="profile img"
+              m="auto"
+            />
+            <Text fontSize="lg" fontWeight="extrabold">
+              Name
+            </Text>
+            <Text fontSize="md" color="gray.600">
+              Sub Text
+            </Text>
+          </Stack>
+        </Box>
+      </WrapItem>
+    </Wrap>
+  );
 });
