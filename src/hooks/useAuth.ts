5a094a53607a0229d417be8d2c993f33b4e575ee
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-13 08:14:28 +0900

    プロバイダを実装。ログインしたら保持するように定義。動作確認おk。

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,12 +3,14 @@ import axios from 'axios';
 import { useCallback, useState } from 'react';
 import { useHistory } from 'react-router';
 import { User } from '../types/api/user';
+import { useLoginUser } from './useLoginUser';
 import { useMessage } from './useMessage';
 
 export const useAuth = () => {
   const history = useHistory();
   const { showMessage } = useMessage();
   const [loading, setLoading] = useState(false);
+  const { setLoginUser } = useLoginUser();
 
   const login = useCallback(
     (id: string) => {
@@ -17,16 +19,20 @@ export const useAuth = () => {
         .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
           if (res.data) {
+            setLoginUser(res.data);
             showMessage({ title: 'Success login!', status: 'success' });
             history.push('/home');
           } else {
             showMessage({ title: "Don't find user!", status: 'error' });
+            setLoading(false);
           }
         })
-        .catch(() => showMessage({ title: "Can't login", status: 'error' }))
-        .finally(() => setLoading(false));
+        .catch(() => {
+          showMessage({ title: "Can't login", status: 'error' });
+          setLoading(false);
+        });
     },
-    [showMessage]
+    [showMessage, setLoginUser]
   );
 
   return { login, loading };
--- src/providers/LoginUserProvider.tsx
+++ src/providers/LoginUserProvider.tsx
@@ -7,12 +7,12 @@ import {
 } from 'react';
 import { User } from '../types/api/user';
 
-type LoginUserContextType = {
+export type LoginUserContextType = {
   loginUser: User | null;
   setLoginUser: Dispatch<SetStateAction<User | null>>;
 };
 
-const LoginUserContext = createContext<LoginUserContextType>(
+export const LoginUserContext = createContext<LoginUserContextType>(
   {} as LoginUserContextType
 );
 
